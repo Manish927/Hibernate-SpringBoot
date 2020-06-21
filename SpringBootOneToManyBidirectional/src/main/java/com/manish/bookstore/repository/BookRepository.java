9d39a527690fc6ec5018e68cc4692a429106b146
Manish Srivastava <mannu927@yahoo.com>
2020-06-21 16:07:02 +0530

    Create BookRepository.java

---
+++ SpringBootOneToManyBidirectional/src/main/java/com/manish/bookstore/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.manish.bookstore.repository;
+
+import com.manish.bookstore.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
