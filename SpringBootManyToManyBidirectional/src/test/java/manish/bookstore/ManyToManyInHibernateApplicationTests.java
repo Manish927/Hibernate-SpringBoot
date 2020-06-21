0cac529fdcf4aab568797c7a5a93bf8b69d1a060
Manish Srivastava <mannu927@yahoo.com>
2020-06-21 19:06:47 +0530

    Create ManyToManyInHibernateApplicationTests.java

---
+++ SpringBootManyToManyBidirectional/src/test/java/manish/bookstore/ManyToManyInHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package manish.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyInHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
