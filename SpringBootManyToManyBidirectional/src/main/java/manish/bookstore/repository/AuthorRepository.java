09eb97f2ae7feeeee53fe6d17ead70f8f22bc925
Manish Srivastava <mannu927@yahoo.com>
2020-06-21 19:02:20 +0530

    Create AuthorRepository.java

---
+++ SpringBootManyToManyBidirectional/src/main/java/manish/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package manish.bookstore.repository;
+
+import manish.bookstore.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
