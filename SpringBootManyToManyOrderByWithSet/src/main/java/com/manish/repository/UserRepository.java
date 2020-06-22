448bd781d77d14f26f3d68bb525153e68dc63081
Manish Srivastava <mannu927@yahoo.com>
2020-06-22 16:56:07 +0530

    Database extension repository

--- SpringBootManyToManyOrderByWithSet/src/main/java/com/manish/repository/UserRepository.java
+++ SpringBootManyToManyOrderByWithSet/src/main/java/com/manish/repository/UserRepository.java
@@ -1,1 +1,10 @@
 
+package com.manish.repository;
+
+import com.manish.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
