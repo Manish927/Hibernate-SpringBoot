0b38711601bc234928654b9b3a2c82b52fbf9ef5
Manish Srivastava <mannu927@yahoo.com>
2020-06-22 16:52:27 +0530

    Update Device.java

--- SpringBootManyToManyOrderByWithSet/src/main/java/com/manish/entity/Device.java
+++ SpringBootManyToManyOrderByWithSet/src/main/java/com/manish/entity/Device.java
@@ -1,1 +1,102 @@
 
+package com.manish.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Device implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String model;
+    private String architecture;
+    private Date launch;
+
+    @ManyToMany(mappedBy = "devices")
+    @OrderBy("name DESC")
+    private Set<User> users = new HashSet<>();
+
+
+    public void setUsers(Set<User> users)
+    {
+        this.users = users;
+    }
+
+    public Set<User> getUsers()
+    {
+        return users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Device device = (Device) o;
+        return id != null && id.equals(device.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 2020;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                ", architecture='" + architecture + '\'' +
+                ", launch=" + launch +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getArchitecture() {
+        return architecture;
+    }
+
+    public void setArchitecture(String architecture) {
+        this.architecture = architecture;
+    }
+
+    public Date getLaunch() {
+        return launch;
+    }
+
+    public void setLaunch(Date launch) {
+        this.launch = launch;
+    }
+}
