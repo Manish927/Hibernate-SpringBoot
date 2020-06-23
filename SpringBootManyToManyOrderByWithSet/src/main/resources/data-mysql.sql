32fdd2d2577acd5b2fb19ad812e88340cda9993c
Manish Srivastava <mannu927@yahoo.com>
2020-06-23 11:37:52 +0530

    Sample mysql data

--- SpringBootManyToManyOrderByWithSet/src/main/resources/data-mysql.sql
+++ SpringBootManyToManyOrderByWithSet/src/main/resources/data-mysql.sql
@@ -1,1 +1,13 @@
 
+-- insert Users
+INSERT INTO `user` (`age`, `name`, `address`, `id`) VALUES (23, "Mark Janel", "23 Workhaven Lane", 1);
+INSERT INTO `user` (`age`, `name`, `address`, `id`) VALUES (43, "Olivia Goy", "1411 Lillydale Drive", 2);
+INSERT INTO `user` (`age`, `name`, `address`, `id`) VALUES (51, "Quartis Young", "43 Vinilus Manor", 3);
+INSERT INTO `user` (`age`, `name`, `address`, `id`) VALUES (32, "Joana Nimar", "548 Uruapan Street", 4);
+
+
+-- insert Devices
+INSERT INTO `device` (`name`, `model`, `architecture`, `launch`, `id`) VALUES ("iPhone 6s", "001-JN", "IOS x86_64", "2016-1-30", 1);
+INSERT INTO `device` (`name`, `model`, `architecture`, `launch`, `id`) VALUES ("samsung tablet", "002-JN", "Andriod x86_64", "2016-1-30", 2);
+INSERT INTO `device` (`name`, `model`, `architecture`, `launch`, `id`) VALUES ("iPhone 7", "001-MJ", "IOS x86_64", "2017-1-30", 3);
+INSERT INTO `device` (`name`, `model`, `architecture`, `launch`, `id`) VALUES ("iPhone 8", "001-OG", "IOS x86_64", "2018-1-30", 4);
